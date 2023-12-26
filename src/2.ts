e6bde8ea074e55e105c24344e659366288c075d9
mariana9uk <maryanagolyuk@gmail.com>
2023-12-26 21:18:37 +0200

    done

--- src/2.ts
+++ src/2.ts
@@ -16,14 +16,14 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  // name: string;
+  // department: string;
+  // salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(public name: string, private department: string, protected salary: number) {
+    // this.name = name;
+    // this.department = department;
+    // this.salary = salary;
   }
 
   getEmployeeDetails() {
@@ -32,6 +32,11 @@ class Employee {
 }
 
 class Manager extends Employee {
+
+  constructor(name: string, department: string, salary:number){
+    super(name, department, salary)
+    this.salary+=10000
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
 
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,52 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+  public getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(door: boolean, key: Key) {
+    this.door = door;
+    this.key = key;
+    this.tenants = [];
+  }
+  public abstract openDoor(key: Key): void;
+
+  comeIn(tenant: Person): void {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+}
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(false, key);
+  }
+  public openDoor(key: Key): void {
+    if (key.getSignature === this.key.getSignature) {
+      this.door = true;
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
